6c0ff32aa9b9e9f9d689abaf9c1495cec0d3daac
linshirui2020 <2419314401@qq.com>
2020-06-11 09:26:46 +0800

    提交到本地仓库

---
+++ gitproject/src/comstudent/model/StudentModel.java
@@ -0,0 +1,17 @@
+package comstudent.model;
+
+public class StudentModel {
+	private int id;
+	private String username;
+	private String password;
+	private int sex;
+	
+	   
+	public StudentModel(int id,String username,String password,int sex) {
+		 super();
+		 this.id=id;
+		 this.username=username;
+		 this.password=password;
+		 this.sex=sex;
+	   }
+}
